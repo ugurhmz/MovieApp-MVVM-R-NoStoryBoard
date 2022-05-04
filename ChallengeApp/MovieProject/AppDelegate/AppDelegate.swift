1ae946c3cdd50beea858baa31a91078e42bf6475
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-05-04 03:54:19 +0300

    Routing added then HomeVC shown success

--- ChallengeApp/MovieProject/AppDelegate/AppDelegate.swift
+++ ChallengeApp/MovieProject/AppDelegate/AppDelegate.swift
@@ -12,12 +12,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        app.router.start()
-        
-        return true
-    }
+    var window: UIWindow?
+
+      func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+          // Override point for customization after application launch.
+          app.router.start()
+          return true
+      }
 
     
 }
