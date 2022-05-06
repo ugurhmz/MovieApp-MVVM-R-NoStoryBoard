49adba707c25a6812d729f4afe663edea694a82c
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-05-06 07:30:14 +0300

    PodFile.lock added

---
+++ ChallengeApp/DataProvider/DataProvider/DataProvider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataProvider
+//
+//  Created by ugur-pc on 6.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
