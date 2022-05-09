1cb73d81a47eab80ed4cbc11f36f5eaf83a93ca1
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-05-09 13:10:38 +0300

    ReusableView edited

--- ChallengeApp/UIComponents/Cell/DetailCells/HeaderReusableView.swift
+++ ChallengeApp/UIComponents/Cell/DetailCells/HeaderReusableView.swift
@@ -10,38 +10,29 @@ import UIKit
 class HeaderReusableView: UICollectionReusableView {
     static var identifier = "HeaderReusableView"
        
-       
        public let titleLabel: UILabel = {
            let label = UILabel()
            label.font = .systemFont(ofSize: 19, weight: .semibold)
            label.text = "ABC"
            label.textColor = .black
-           label.translatesAutoresizingMaskIntoConstraints = false
            return label
        }()
        
            override init(frame: CGRect) {
                super.init(frame: frame)
-              
                addSubview(titleLabel)
-               
                setConstraints()
            }
 
            required init(coder aDecoder: NSCoder) {
                super.init(coder: aDecoder)!
-               
            }
        
        func setConstraints(){
-           NSLayoutConstraint.activate([
-               titleLabel.topAnchor.constraint(equalTo: topAnchor),
-               titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
-               titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
-               titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
-               
-               
-           ])
+           titleLabel.anchor(top: topAnchor,
+                             leading: leadingAnchor,
+                             bottom: bottomAnchor,
+                             trailing: trailingAnchor,
+                             padding: .init(top: 0, left: 5, bottom: 0, right: 0))
        }
-      
 }
